d9b8a7508de49bcd7eaa943ed9f0aebbf8730d2d
devanshk <devanshkukreja@Devanshs-MBP.wv.cc.cmu.edu>
2017-04-24 19:59:05 -0400

    Revert "Merge pull request #1 from thepavangollapalli/Article"
    This reverts commit 8267f0b0ce995c15b567ad335f0fafa2c3749174, reversing
    changes made to 4e0be62fecbebcaf36c01d953cdf8d9b494204fd.

--- app/models/article.rb
+++
@@ -1,8 +0,0 @@
-class Article < ActiveRecord::Base
-	belongs_to :category
-
-	validates_presence_of :title, :content
-
-	scope :active, where('active = ?', true)
-  	scope :alphabetical, order('title')
-end
